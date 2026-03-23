6e9bcb426e50b347e8cf1e74337f9d0520346830
E Rithvik <rithvikrithvik54@gmail.com>
2026-03-24 00:37:09 +0530

    Update PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,8 +1,46 @@
-public class PalindromeCheckerApp {
+import java.util.Scanner;
+
+public class UseCase11PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Palindrome Checker Application Started");
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("===== UC11 : OOP Palindrome Service =====");
+        System.out.print("Enter a string: ");
+
+        String input = sc.nextLine();
+
+        PalindromeService service = new PalindromeService();
+
+        boolean result = service.checkPalindrome(input);
+
+        if (result)
+            System.out.println("Result : Palindrome ✅");
+        else
+            System.out.println("Result : Not Palindrome ❌");
+
+        sc.close();
+    }
+}
+
+/* Service class */
+class PalindromeService {
+
+    public boolean checkPalindrome(String input) {
+
+        int start = 0;
+        int end = input.length() - 1;
+
+        while (start < end) {
+
+            if (input.charAt(start) != input.charAt(end))
+                return false;
+
+            start++;
+            end--;
+        }
+
+        return true;
     }
 }
