3c355d50b4142a4696ffa6be458e764109184c5c
E Rithvik <rithvikrithvik54@gmail.com>
2026-03-24 00:33:34 +0530

    Update PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,8 +1,40 @@
-public class PalindromeCheckerApp {
+import java.util.Scanner;
+
+public class UseCase10PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Palindrome Checker Application Started");
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("===== UC10 : Normalized Palindrome Check =====");
+        System.out.print("Enter a string: ");
+
+        String input = sc.nextLine();
+
+        // normalization
+        input = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        int start = 0;
+        int end = input.length() - 1;
+
+        boolean isPalindrome = true;
+
+        while (start < end) {
+
+            if (input.charAt(start) != input.charAt(end)) {
+                isPalindrome = false;
+                break;
+            }
+
+            start++;
+            end--;
+        }
+
+        if (isPalindrome)
+            System.out.println("Result : Palindrome ✅");
+        else
+            System.out.println("Result : Not Palindrome ❌");
+
+        sc.close();
     }
 }
