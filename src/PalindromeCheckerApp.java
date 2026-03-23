d9efe44379216778bb30d4b487c02bc3293082f6
E Rithvik <rithvikrithvik54@gmail.com>
2026-03-24 00:14:33 +0530

    Refactor PalindromeCheckerApp to Use Queue and Stack

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,8 +1,45 @@
-public class PalindromeCheckerApp {
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class UseCase6PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Palindrome Checker Application Started");
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("===== UC6 : Queue + Stack Palindrome Check =====");
+        System.out.print("Enter a string: ");
+
+        String input = sc.nextLine();
+
+        Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
+
+        // insert into both structures
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            stack.push(ch);
+            queue.add(ch);
+        }
+
+        boolean isPalindrome = true;
+
+        // compare
+        for (int i = 0; i < input.length(); i++) {
+
+            if (stack.pop() != queue.remove()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome)
+            System.out.println("Result : Palindrome ✅");
+        else
+            System.out.println("Result : Not Palindrome ❌");
+
+        sc.close();
     }
 }
