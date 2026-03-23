99f9b9aabe297025efa7b65a2b364a2d45273abe
E Rithvik <rithvikrithvik54@gmail.com>
2026-03-24 00:50:14 +0530

    Update PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,8 +1,48 @@
-public class PalindromeCheckerApp {
+import java.util.Scanner;
+
+public class UseCase13PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Palindrome Checker Application Started");
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("===== UC13 : Palindrome Performance Comparison =====");
+        System.out.print("Enter a string: ");
+
+        String input = sc.nextLine();
+
+        long startTime = System.nanoTime();
+
+        boolean result = checkPalindrome(input);
+
+        long endTime = System.nanoTime();
+
+        long duration = endTime - startTime;
+
+        if (result)
+            System.out.println("Result : Palindrome ✅");
+        else
+            System.out.println("Result : Not Palindrome ❌");
+
+        System.out.println("Execution Time (nanoseconds): " + duration);
+
+        sc.close();
+    }
+
+    private static boolean checkPalindrome(String s) {
+
+        int start = 0;
+        int end = s.length() - 1;
+
+        while (start < end) {
+
+            if (s.charAt(start) != s.charAt(end))
+                return false;
+
+            start++;
+            end--;
+        }
+
+        return true;
     }
 }
