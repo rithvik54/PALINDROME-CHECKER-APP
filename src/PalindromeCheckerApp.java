e21713f6d637e48114ee134b0c91e017a96836a3
rithvik54 <rithvikrithvik54@gmail.com>
2026-03-24 11:05:06 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,3 +1,31 @@
-public class PalindromeCheckerApp {
+ {public class UseCase2PalindromeCheckerApp {
 
+    public static void main(String[] args) {
+
+        String word = "madam";   // hardcoded string
+
+        boolean isPalindrome = true;
+
+        int start = 0;
+        int end = word.length() - 1;
+
+        while (start < end) {
+
+            if (word.charAt(start) != word.charAt(end)) {
+                isPalindrome = false;
+                break;
+            }
+
+            start++;
+            end--;
+        }
+
+        if (isPalindrome) {
+            System.out.println(word + " is a Palindrome ✅");
+        } else {
+            System.out.println(word + " is Not a Palindrome ❌");
+        }
+    }
 }
+
+
