e0240c481fc036d3ecac3a214e997d6bd9f48415
rithvik54 <rithvikrithvik54@gmail.com>
2026-03-24 11:01:16 +0530

    UC1 code added

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,3 +1,16 @@
-public class PalindromeCheckerApp {
+public class UseCase1PalindromeCheckerApp {
 
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println("   PALINDROME CHECKER APPLICATION   ");
+        System.out.println("====================================");
+
+        System.out.println("Welcome to Palindrome Checker System");
+        System.out.println("Version : 1.0");
+
+        System.out.println("Application Started Successfully 👍");
+    }
 }
+
+
